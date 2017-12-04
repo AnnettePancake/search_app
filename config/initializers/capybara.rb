d9b189c657f896657ef3527f7fd90e7a95589ea6
Anna <annette.pancake@gmail.com>
2017-12-04 19:29:22 +0300

    [phonearena_service] find dimensions logic

---
+++ config/initializers/capybara.rb
@@ -0,0 +1,6 @@
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, js_errors: false, debug: false)
+end
+
+Capybara.default_driver = :poltergeist
+Capybara.javascript_driver = :poltergeist
